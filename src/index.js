611e5558f8638363ed1d9ef8c971324fec6488fe
Takunda Nyamatore <tjnyamatore@gmail.com>
2022-02-06 13:27:57 +0000

    site template built

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "./styles/general.css";
 import "./styles/hero.css";
 import "./styles/about-me.css";
 import "./styles/contact.css";
+import "./styles/pages.css";
 
 import "./TJs-UI-Library/styles/navbar.css";
 import "./TJs-UI-Library/styles/dropdown.css";
@@ -46,4 +47,45 @@ const tooltips = document.querySelectorAll(".tooltip");
 tooltips.forEach((tooltip) => {
   const instance = new Tooltip(tooltip);
   instance.init();
+});
+
+// contact form
+
+// Send us a message functionality
+const contactForm = document.querySelector(".contact-me");
+const contactFormRepsonse = document.querySelector(".contact-form-response");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // https://github.com/github/fetch
+  fetch("https://formsubmit.co/ajax/tjnyamatore@gmail.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: contactForm.name.value.trim(),
+      email: contactForm.email.value.trim(),
+      message: contactForm.message.value.trim(),
+    }),
+  })
+    .then((response) => {
+      response.json();
+      if (response.ok) {
+        contactFormRepsonse.style.display = "block";
+        contactFormRepsonse.style.color = "white";
+        contactFormRepsonse.textContent =
+          "Thank you, your message has been sent!";
+      } else {
+        contactFormRepsonse.style.display = "block";
+        contactFormRepsonse.style.color = "red";
+        contactFormRepsonse.textContent =
+          "Sorry, there was a problem. please use our email to the left.";
+      }
+    })
+
+    .then((data) => console.log(data))
+
+    .catch((error) => console.log(error));
 });
